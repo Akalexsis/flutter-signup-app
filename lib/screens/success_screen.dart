51ea9f6a256022e9e37f0dec752a220fa8a277d5
Kayla Thornton <kaylathornton@Kaylas-Air.wifi.gsu.edu>
2026-03-30 18:39:03 -0400

    init: initialize success screen

---
+++ lib/screens/success_screen.dart
@@ -0,0 +1,9 @@
+/* 
+    Author - Kayla Thornton
+    Purpose - Signup form for users to create a new account
+ */
+import 'package:flutter/material.dart';
+
+class SuccessPage extends StatelessWidget {
+  const SuccessPage({super.key});
+} 
